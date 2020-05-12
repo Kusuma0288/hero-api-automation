f5150f28a8568e121b2bef923ded59a9a0688a5c
vanantha-woolies <vanantha@woolworths.com.au>
2020-05-12 14:48:47 +1000

    Deletion of items from trolley
    Support for deleting items from V2 and V3 trolley.

---
+++ src/test/java/au/com/woolworths/apigee/model/TrolleyDetails.java
@@ -0,0 +1,97 @@
+package au.com.woolworths.apigee.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class TrolleyDetails {
+	private int subtotalInclDelivery;
+	private int savingsOnTrolley;
+	private int totalProducts;
+	private int deliveryFee;
+	private List <TrolleyItemsListResponse> items;
+	private Object wowRewardsSummary;
+	private Object loyalty;
+	
+
+    @Override
+    public String toString() {
+        return "TrolleyResults{" +
+        		"SubtotalInclDelivery=" + subtotalInclDelivery +
+        		", SavingsOnTrolley=" + savingsOnTrolley +
+        		", TotalProducts=" + totalProducts +
+        		", DeliveryFee=" + deliveryFee +
+                ",items=" + items +
+                ",WowRewardsSummary=" + wowRewardsSummary +
+                ",Loyalty=" + loyalty +
+                '}';
+    }
+
+	public int getSubtotalInclDelivery() {
+		return subtotalInclDelivery;
+	}
+
+	public void setSubtotalInclDelivery(int subtotalInclDelivery) {
+		this.subtotalInclDelivery = subtotalInclDelivery;
+	}
+
+	public int getSavingsOnTrolley() {
+		return savingsOnTrolley;
+	}
+
+	public void setSavingsOnTrolley(int savingsOnTrolley) {
+		this.savingsOnTrolley = savingsOnTrolley;
+	}
+
+	public int getTotalProducts() {
+		return totalProducts;
+	}
+
+	public void setTotalProducts(int totalProducts) {
+		this.totalProducts = totalProducts;
+	}
+
+	public int getDeliveryFee() {
+		return deliveryFee;
+	}
+
+	public void setDeliveryFee(int deliveryFee) {
+		this.deliveryFee = deliveryFee;
+	}
+
+	
+	public List<TrolleyItemsListResponse> getTrolleyitemsListResp() {
+		return items;
+	}
+
+	public void setTrolleyitemsListResp(List<TrolleyItemsListResponse> trolleyitemsListResp) {
+		this.items = trolleyitemsListResp;
+	}
+
+	public Object getWowRewardsSummary() {
+		return wowRewardsSummary;
+	}
+
+	public Object getItems() {
+		return items;
+	}
+
+	public void setWowRewardsSummary(Object wowRewardsSummary) {
+		this.wowRewardsSummary = wowRewardsSummary;
+	}
+
+	public Object getLoyalty() {
+		return loyalty;
+	}
+
+	public void setLoyalty(Object loyalty) {
+		this.loyalty = loyalty;
+	}
+
+	
+
+	
+
+    
+}
