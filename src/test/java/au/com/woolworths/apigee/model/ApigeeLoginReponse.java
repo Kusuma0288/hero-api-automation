f448fcb60e3786de6fc5f4ce331c281ae8cf4cc2
Anand Murthy <amurthy1@woolworths.com.au>
2020-06-22 11:40:58 +1000

    Removed Json property tags

--- src/test/java/au/com/woolworths/apigee/model/ApigeeLoginReponse.java
+++ src/test/java/au/com/woolworths/apigee/model/ApigeeLoginReponse.java
@@ -3,69 +3,38 @@ package au.com.woolworths.apigee.model;
 import lombok.Data;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
 @Data public class ApigeeLoginReponse {
-  @JsonProperty("access_token")
-  private String accessToken;
-  @JsonProperty("refresh_token")
-  private String refreshToken;
-  @JsonProperty("expires_in")
-  private String expiresIn;
-  @JsonProperty("trolleyhref")
+  private String access_token;
+  private String refresh_token;
+  private String expires_in;
   private String trolleyhref;
-  @JsonProperty("fulfilmenthref")
   private String fulfilmenthref;
-  @JsonProperty("paymenthref")
   private String paymenthref;
-  @JsonProperty("shopperid")
   private String shopperid;
-  @JsonProperty("title")
   private String title;
-  @JsonProperty("firstname")
   private String firstname;
-  @JsonProperty("lastname")
   private String lastname;
-  @JsonProperty("email")
   private String email;
-  @JsonProperty("dateofbirth")
   private String dateofbirth;
-  @JsonProperty("primaryphone")
   private String primaryphone;
-  @JsonProperty("secondaryphone")
   private String secondaryphone;
-  @JsonProperty("otherphone")
   private String otherphone;
-  @JsonProperty("fulfilmentstoreid")
   private String fulfilmentstoreid;
-  @JsonProperty("fulfilmentmethod")
   private String fulfilmentmethod;
-  @JsonProperty("fulfilmentstorename")
   private String fulfilmentstorename;
-  @JsonProperty("deliverymethod")
   private String deliverymethod;
-  @JsonProperty("deliverystoreaddressid")
   private String deliverystoreaddressid;
-  @JsonProperty("deliverystoreaddresscity")
   private String deliverystoreaddresscity;
-  @JsonProperty("deliverystoreaddresscountry")
   private String deliverystoreaddresscountry;
-  @JsonProperty("deliveryaddressstreet1")
   private String deliveryaddressstreet1;
-  @JsonProperty("deliveryaddressstreet2")
   private String deliveryaddressstreet2;
-  @JsonProperty("deliveryaddresssuburb")
   private String deliveryaddresssuburb;
-  @JsonProperty("deliveryinstructions")
   private String deliveryinstructions;
-  @JsonProperty("isservicable")
   private String isservicable;
-  @JsonProperty("rewardscardNumber")
   private String rewardscardNumber;
-  @JsonProperty("teammember")
   private String teammember;
-  @JsonProperty("trolleymergestatus")
   private String trolleymergestatus;
   private String statusCode;
   private String error;
--- src/test/java/au/com/woolworths/apigee/stepdefinitions/ApigeeShopperDefinition.java
+++ src/test/java/au/com/woolworths/apigee/stepdefinitions/ApigeeShopperDefinition.java
@@ -29,7 +29,7 @@ public class ApigeeShopperDefinition extends ApigeeShopperHelper {
 
     sharedData.responseStatusCode = response.getStatusCode();
     sharedData.shopperResponse = response;
-    sharedData.accessToken = response.getAccessToken();
+    sharedData.accessToken = response.getAccess_token();
   }
 
   @When("^user continue to connect to apigee with login (.*) and password (.*)$")
@@ -43,15 +43,15 @@ public class ApigeeShopperDefinition extends ApigeeShopperHelper {
   public void userFailsToAuthenticateAsShopper() throws Throwable {
     Assert.assertTrue(sharedData.responseStatusCode.equals("401"), "Status Code is::" + sharedData.responseStatusCode);
     Assert.assertTrue(sharedData.shopperResponse.getError().equals("The email address & password combination you have entered is incorrect.  Please try again or click the forgotten password link below to reset your password."), "Error Message is::" + sharedData.shopperResponse.getError());
-    Assert.assertNull(sharedData.shopperResponse.getAccessToken(), "Access Token is NOT NULL");
-    Assert.assertNull(sharedData.shopperResponse.getRefreshToken(), "Refresh Token is NOT NULL");
+    Assert.assertNull(sharedData.shopperResponse.getAccess_token(), "Access Token is NOT NULL");
+    Assert.assertNull(sharedData.shopperResponse.getRefresh_token(), "Refresh Token is NOT NULL");
   }
 
   @Then("^user is successfully authenticated with all session details$")
   public void userIsSuccessfullyAuthenticatedWithAllSessionDetails() throws Throwable {
     Assert.assertTrue(sharedData.shopperResponse.getStatusCode().equals("200"), "Expected Status Code is 200 but found::" + sharedData.responseStatusCode);
-    Assert.assertNotNull(sharedData.shopperResponse.getAccessToken(), "Access Token is Null");
-    Assert.assertNotNull(sharedData.shopperResponse.getRefreshToken(), "Refresh Token is Null");
+    Assert.assertNotNull(sharedData.shopperResponse.getAccess_token(), "Access Token is Null");
+    Assert.assertNotNull(sharedData.shopperResponse.getRefresh_token(), "Refresh Token is Null");
 
   }
 
@@ -66,8 +66,8 @@ public class ApigeeShopperDefinition extends ApigeeShopperHelper {
   public void userFailsToAuthenticateAsAPIKeyIsInvalid() throws Throwable {
     Assert.assertTrue(sharedData.shopperResponse.getStatusCode().equals("401"), "Expected Status Code is 401 but found::" + sharedData.responseStatusCode);
     Assert.assertTrue(sharedData.shopperResponse.getError().equals("invalid_client"), "API error is not coming as expected (invalid_client");
-    Assert.assertNull(sharedData.shopperResponse.getAccessToken(), "Access Token is Not Null");
-    Assert.assertNull(sharedData.shopperResponse.getRefreshToken(), "Refresh Token is Not Null");
+    Assert.assertNull(sharedData.shopperResponse.getAccess_token(), "Access Token is Not Null");
+    Assert.assertNull(sharedData.shopperResponse.getRefresh_token(), "Refresh Token is Not Null");
   }
 
   @When("^user continue to connect to apigee with login containing (.*)$")
@@ -75,7 +75,7 @@ public class ApigeeShopperDefinition extends ApigeeShopperHelper {
     ApigeeLoginReponse response = userToConnectApigeewithLoginAndPassword(userName + "@" + TestProperties.get("EMAIL_DOMAINNAME"), TestProperties.get("SHOPPER_PASSWORD"), sharedData.deviceId);
     sharedData.responseStatusCode = response.getStatusCode();
     sharedData.shopperResponse = response;
-    sharedData.accessToken = response.getAccessToken();
+    sharedData.accessToken = response.getAccess_token();
 
   }
 
@@ -85,7 +85,7 @@ public class ApigeeShopperDefinition extends ApigeeShopperHelper {
 
     sharedData.responseStatusCode = response.getStatusCode();
     sharedData.shopperResponse = response;
-    sharedData.accessToken = response.getAccessToken();
+    sharedData.accessToken = response.getAccess_token();
   }
 
 }
