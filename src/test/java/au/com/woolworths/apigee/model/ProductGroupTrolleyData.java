b2ccd2f720ba0f777009b23b7c0a44f1b47027aa
Anand Murthy <amurthy1@woolworths.com.au>
2020-06-22 12:12:50 +1000

    Removed Json property tag @default, it is not the same as defaultInTrolley, these are different. default value is sent when running homepage.feature and only appears on some calls.

--- src/test/java/au/com/woolworths/apigee/model/ProductGroupTrolleyData.java
+++ src/test/java/au/com/woolworths/apigee/model/ProductGroupTrolleyData.java
@@ -1,17 +1,17 @@
 package au.com.woolworths.apigee.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Data public class ProductGroupTrolleyData {
   private String minimum;
   private String maximum;
   private String increment;
-  //TODO rename default to default in Trolley
-  @JsonProperty("default")
   private String defaultInTrolley;
   private String inTrolley;
   private String unit;
