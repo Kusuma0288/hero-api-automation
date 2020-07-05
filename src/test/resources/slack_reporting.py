5de4a35723f05710221155f66b8e4971de282405
JasWoolieX <jbamrah@woolworths.com.au>
2020-07-06 08:10:08 +1000

    Formatted code

--- src/test/resources/slack_reporting.py
+++ src/test/resources/slack_reporting.py
@@ -5,8 +5,9 @@ import json
 import sys
 import jsonpath_rw_ext as jp
 
-#from slackclient import SlackClient
+# from slackclient import SlackClient
 import matplotlib
+
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
@@ -15,37 +16,37 @@ workspace = '~/Woolworths/woolworths-mobile-api-automation'
 channel_name = '#testing_python_slack'
 
 if user == 'jenkins':
-	workspace = os.environ['WORKSPACE']
-	channel_name = '#shopapp-auto-reports'
+    workspace = os.environ['WORKSPACE']
+    channel_name = '#shopapp-auto-reports'
 elif user == 'vsts':
-	workspace = '/home/vsts/work/1/s'
-	channel_name = '#shopapp-auto-reports'
+    workspace = '/home/vsts/work/1/s'
+    channel_name = '#shopapp-auto-reports'
 
-path = workspace+'/target/cucumber-reports/advanced-reports/'
+path = workspace + '/target/cucumber-reports/advanced-reports/'
 
 total_no_of_scenarios = 0
 total_scenarios_failed = 0
 
 for filepath in glob.glob(os.path.join(os.path.expanduser(path), '*.json')):
-	#print("The Feature file Path is::"+filepath)
-	with open(filepath) as json_file:
-		json_data = json.load(json_file)
+    # print("The Feature file Path is::"+filepath)
+    with open(filepath) as json_file:
+        json_data = json.load(json_file)
 
-		individual_scenarios = jp.match1("$.[*].elements",json_data)
-		total_no_of_scenarios += len(individual_scenarios)
-		total_scenarios_failed +=len(jp.match("$.[*].elements[?(steps[*].result.status~'.*failed.*')]",json_data))
+        individual_scenarios = jp.match1("$.[*].elements", json_data)
+        total_no_of_scenarios += len(individual_scenarios)
+        total_scenarios_failed += len(jp.match("$.[*].elements[?(steps[*].result.status~'.*failed.*')]", json_data))
 
-#print "\n\nThe Total Number of Scenarios::",total_no_of_scenarios
-#print "\n\nFailed Scenarios::",total_scenarios_failed
+# print "\n\nThe Total Number of Scenarios::",total_no_of_scenarios
+# print "\n\nFailed Scenarios::",total_scenarios_failed
 
-sizes = [total_no_of_scenarios-total_scenarios_failed,total_scenarios_failed]
+sizes = [total_no_of_scenarios - total_scenarios_failed, total_scenarios_failed]
 
-labels = ('Passed: '+str((total_no_of_scenarios-total_scenarios_failed)), 'Failed: '+ str(total_scenarios_failed))
+labels = ('Passed: ' + str((total_no_of_scenarios - total_scenarios_failed)), 'Failed: ' + str(total_scenarios_failed))
 colors = ['#58FB4C', '#F8183A']
 explode = (0.1, 0)
-plt.pie(sizes, explode=explode,labels=labels,colors=colors, autopct='%1.1f%%',shadow=True, startangle=90)
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=90)
 plt.title("PUBLIC TRADER API - REPORT")
-filename = workspace+'/target/cucumber-reports/advanced-reports/test.png'
+filename = workspace + '/target/cucumber-reports/advanced-reports/test.png'
 plt.savefig(os.path.expanduser(filename))
 
 '''
