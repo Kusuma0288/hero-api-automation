f061a8283dcfb7c7dc0b477c4491211bba708e29
JasWoolieX <jbamrah@woolworths.com.au>
2020-07-22 11:33:59 +1000

    Update base template

--- src/test/resources/squad_counts.ps1
+++ src/test/resources/squad_counts.ps1
@@ -1,3 +1,10 @@
 Param([string]$ServerName)
 echo "in squad count script"
 echo $ServerName
+$webclient = New-Object System.Net.WebClient
+$html = $webclient.DownloadString($wolvesURL)
+$Regex = [Regex]::Matches($html, $pattern)
+$newVal = $Regex.value
+$pattern11='<i[^>]*>(?:.|\n)+?</i>'
+$Regex1 = [Regex]::Matches($newVal, $pattern11)
+return $Regex1.value.split('title="passed"></i>')[1].Trim().Substring(0,1)
