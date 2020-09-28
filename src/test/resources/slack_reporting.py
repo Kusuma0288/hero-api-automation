9e9aa6d0e80deeb7279bacf23dbf0153bc06a6ca
JasWoolieX <jbamrah@woolworths.com.au>
2020-09-28 23:07:44 +1000

    Update slack_reporting.py

--- src/test/resources/slack_reporting.py
+++ src/test/resources/slack_reporting.py
@@ -22,7 +22,7 @@ elif user == 'vsts':
     workspace = '/home/vsts/work/1/s'
     channel_name = '#azureautomation'
 elif user == 'AzDevOps':
-    workspace = '/home/vsts/work/1/s'
+    workspace = '/home/AzDevOps'
     channel_name = '#azureautomation'
 
 
