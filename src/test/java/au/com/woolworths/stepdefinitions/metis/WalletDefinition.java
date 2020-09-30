372e45eb2115cc4ce67a0b6707f3eb9c1c130542
Lewis O'Brien <lobrien1@woolworths.com.au>
2020-09-30 13:39:44 +1000

    Assert wallet state

--- src/test/java/au/com/woolworths/stepdefinitions/metis/WalletDefinition.java
+++ src/test/java/au/com/woolworths/stepdefinitions/metis/WalletDefinition.java
@@ -1,23 +1,34 @@
 package au.com.woolworths.stepdefinitions.metis;
 
-import au.com.woolworths.helpers.metis.LoginHelper;
+import au.com.woolworths.graphql.parser.GraphqlParser;
+import au.com.woolworths.helpers.metis.RewardsCardWithWalletHelper;
+import au.com.woolworths.model.metis.card.RewardsCardHomePageWithWalletResponse;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import junit.framework.Assert;
 
-public class WalletDefinition extends LoginHelper {
+import java.io.InputStream;
+
+public class WalletDefinition extends RewardsCardWithWalletHelper {
+
+  private RewardsCardHomePageWithWalletResponse rewardsCardHomePageWithWalletResponse;
 
   @When("^the user goes to the card screen$")
-  public void goesToCardScreen() {
+  public void goesToCardScreen() throws Throwable {
+    InputStream iStream = WalletDefinition.class.getResourceAsStream("/gqlQueries/metis/rewardsCardHomePageWithWallet.graphql");
+    String graphqlQuery = GraphqlParser.parseGraphql(iStream, null);
 
+    rewardsCardHomePageWithWalletResponse = iRetrieveMyRewardsCardWithWallet(graphqlQuery);
   }
 
   @Then("^the user should see the wallet is empty$")
   public void shouldSeeEmptyWallet() {
-
+    Assert.assertEquals("Login was unsuccessful", "Add a bank card and redeem Everyday Rewards in one easy tap", rewardsCardHomePageWithWalletResponse.getData().getWalletHomePage().getContent());
+    Assert.assertEquals("Login was unsuccessful", "ADD_CARD", rewardsCardHomePageWithWalletResponse.getData().getWalletHomePage().getAction());
   }
 
   @Then("^the user should be able to add a new card$")
   public void canAddNewCard() {
-
+    // In progress
   }
 }
