14a4ef48a93e7dc96fe291e1a5038be598791bbd
vanantha-woolies <vanantha@woolworths.com.au>
2020-10-23 10:08:06 +1100

    Update Changes to response file

--- src/test/java/au/com/woolworths/model/iris/graphql/changeMyOrder/ChangeMyOrderResponse.java
+++ src/test/java/au/com/woolworths/model/iris/graphql/changeMyOrder/ChangeMyOrderResponse.java
@@ -1,15 +1,6 @@
 package au.com.woolworths.model.iris.graphql.changeMyOrder;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-//import lombok.Data;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonInclude;
-
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.experimental.Accessors;
 
 @lombok.Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
