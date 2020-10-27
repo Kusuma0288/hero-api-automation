380e035026634ac7a372927d2d9458d7deab255e
vanantha-woolies <vanantha@woolworths.com.au>
2020-10-27 15:45:17 +1100

    Updated the ProductDetailsFiles

--- src/test/java/au/com/woolworths/model/iris/graphql/productDetails/ProductDetails.java
+++ src/test/java/au/com/woolworths/model/iris/graphql/productDetails/ProductDetails.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-@lombok.Data
+@Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProductDetails {
 
