e665a1640954308d16169f59f3c35039833be1c3
Pramod Tewarie <ptewarie@woolworths.com.au>
2020-10-29 09:34:20 +0000

    Merged PR 25927: CLO-2279-shore-update
    - added content change for shore model
    - updated assertions
    - Merge branch 'develop' into CLO-2279-shore-update

--- src/test/java/au/com/woolworths/model/metis/shorecampaigndetails/Content.java
+++ src/test/java/au/com/woolworths/model/metis/shorecampaigndetails/Content.java
@@ -5,8 +5,9 @@ import lombok.Data;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
-public class Header {
-  private String imageAlt;
-  private String imageUrl;
-  private Content content;
+public class Content {
+  private String credits;
+  private String heading;
+  private String body;
+  private String expiry;
 }
--- src/test/java/au/com/woolworths/stepdefinitions/metis/ShoreCampaignDetailsDefinition.java
+++ src/test/java/au/com/woolworths/stepdefinitions/metis/ShoreCampaignDetailsDefinition.java
@@ -6,6 +6,7 @@ import au.com.woolworths.model.metis.shorecampaigndetails.Footer;
 import au.com.woolworths.model.metis.shorecampaigndetails.Header;
 import au.com.woolworths.model.metis.shorecampaigndetails.Product;
 import au.com.woolworths.model.metis.shorecampaigndetails.RewardsShoreCampaignDetailsResponse;
+import au.com.woolworths.utils.Utilities;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import junit.framework.Assert;
@@ -31,31 +32,33 @@ public class ShoreCampaignDetailsDefinition extends ShoreCampaignHelper {
   @Then("^the user should see the all the collection containers details$")
   public void theUserShouldSeeTheAllTheCollectionContainersDetails() {
     Header header = rewardsShoreCampaignDetailsResponse.getData().getRewardsShoreCampaignDetails().getHeader();
-    Assert.assertNotNull(header.getTitle());
-    Assert.assertNotNull(header.getImageUrl());
-    Assert.assertNotNull(header.getImageAlt());
-    Assert.assertNotNull(header.getBody());
-    Assert.assertNotNull(header.getCredits());
-    Assert.assertNotNull(header.getExpiry());
+    Assert.assertNotNull("shore header is not returned ", header.getContent().getHeading());
+    Assert.assertTrue("shore image is not returned ", header.getImageUrl().contains("shore"));
+    Assert.assertNotNull("shore alternate text not shown ", header.getImageAlt());
+    Assert.assertTrue("shore body text is not correct ", header.getContent().getBody().contains("Container Credit"));
+    Assert.assertTrue("shore credit field is not numeric ", Utilities.isNumeric(header.getContent().getCredits()));
+    Assert.assertNotNull("date is not return for shore ", header.getContent().getExpiry());
 
 
     List<Product> products = rewardsShoreCampaignDetailsResponse.getData().getRewardsShoreCampaignDetails().getProducts();
     for (Product product : products) {
-      Assert.assertNotNull(product.getDescription());
-      Assert.assertNotNull(product.getImageAlt());
-      Assert.assertNotNull(product.getImageUrl());
-      Assert.assertNotNull(product.getPrice());
-      if (product.getRedemptionStatus().getStatus().contains("credits")) {
-        Assert.assertEquals("Colour for the product redemption text is not orange", product.getRedemptionStatus().getColour(), "#FF5200");
+      Assert.assertNotNull("Shore product description is not returned ", product.getDescription());
+      Assert.assertNotNull("Shore alternate text not returned for product ", product.getImageAlt());
+      Assert.assertTrue("Shore image url is not returned for product ", product.getImageUrl().contains("shore"));
+      Assert.assertTrue("price is not numeric ", Utilities.isNumeric(product.getPrice().substring(1)));
+      if (product.getRedemptionStatus().getStatus().contains("enough")) {
+        Assert.assertEquals("Colour for the product redemption text is not green ", product.getRedemptionStatus().getColour(), "#02AC45");
+      } else {
+        Assert.assertEquals("Colour for the product redemption text is not orange ", product.getRedemptionStatus().getColour(), "#FF5200");
       }
     }
 
     Footer footer = rewardsShoreCampaignDetailsResponse.getData().getRewardsShoreCampaignDetails().getFooter();
-    Assert.assertNotNull(footer.getButton().getLabel());
-    Assert.assertEquals("Footer url is not returned", footer.getButton().getUrl(), "https://www.woolworths.com.au");
-    Assert.assertNotNull(footer.getImageAlt());
-    Assert.assertNotNull(footer.getImageUrl());
-    Assert.assertNotNull(footer.getTermsAndConditions());
-    Assert.assertNotNull(footer.getTitle());
+    Assert.assertTrue("Shore footer button is not returned", footer.getButton().getLabel().equalsIgnoreCase("Shop now"));
+    Assert.assertEquals("Shore Footer url is not returned", footer.getButton().getUrl(), "https://www.woolworths.com.au");
+    Assert.assertNotNull("Shore footer alternative image text not returned", footer.getImageAlt());
+    Assert.assertTrue("Shore footer image not returned ", footer.getImageUrl().contains("shore"));
+    Assert.assertNotNull("Footer text is not returned ", footer.getTermsAndConditions());
+    Assert.assertTrue("Footer title Terms and Conditions not returned ",  footer.getTitle().equalsIgnoreCase("Terms and Conditions"));
   }
 }
