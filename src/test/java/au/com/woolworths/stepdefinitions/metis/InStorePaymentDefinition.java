ac88f6ab2bdd77f741da8916ab3cd73f27caf19a
Lewis O'Brien <lobrien1@woolworths.com.au>
2020-10-29 20:25:45 +1100

    Update assertion messages

--- src/test/java/au/com/woolworths/stepdefinitions/metis/InStorePaymentDefinition.java
+++ src/test/java/au/com/woolworths/stepdefinitions/metis/InStorePaymentDefinition.java
@@ -35,11 +35,11 @@ public class InStorePaymentDefinition extends RewardsCardWithWalletHelper {
   public void userGoesToScan() throws IOException {
     QRIDResponse qridResponse = scanQRCode(qrID);
 
-    Assert.assertNotNull("Location value is missing", qridResponse.getData().getLocation());
-    Assert.assertNotNull("Expiry time value is missing", qridResponse.getData().getExpiryTime());
-    Assert.assertNotNull("Merchant ID value is missing", qridResponse.getData().getMerchantId());
+    Assert.assertNotNull("Location value is null", qridResponse.getData().getLocation());
+    Assert.assertNotNull("Expiry time value is null", qridResponse.getData().getExpiryTime());
+    Assert.assertNotNull("Merchant ID value is null", qridResponse.getData().getMerchantId());
 
-    Assert.assertEquals("Rewards ID number is not as expected", sharedData.rewardsCard, qridResponse.getData().getCustomerInfo().getPayload().getRewardsId());
+    Assert.assertEquals("Customer info rewards ID number is not as expected", sharedData.rewardsCard, qridResponse.getData().getCustomerInfo().getPayload().getRewardsId());
     Assert.assertEquals("Customer info schema ID is not as expected", 36, qridResponse.getData().getCustomerInfo().getSchemaId().length());
 
     Assert.assertNotNull("Merchant info lane ID is missing", qridResponse.getData().getMerchantInfo().getPayload().getLaneId());
@@ -71,26 +71,26 @@ public class InStorePaymentDefinition extends RewardsCardWithWalletHelper {
   private void getTransactionDetails() throws IOException {
     TransactionsResponse transactionsResponse = getTransactionDetails(paymentRequestId);
 
-    Assert.assertEquals("Rewards ID number is not as expected", "APPROVED", transactionsResponse.getData().getTransactions()[0].getStatus());
-    Assert.assertEquals("Rewards ID number is not as expected", "ScanGoSuper", transactionsResponse.getData().getTransactions()[0].getMerchantId());
-    Assert.assertEquals("Rewards ID number is not as expected", Integer.parseInt(TestProperties.get("DIGIPAY_TRANSACTION_AMOUNT")), transactionsResponse.getData().getTransactions()[0].getGrossAmount());
+    Assert.assertEquals("Transaction status is not as expected", "APPROVED", transactionsResponse.getData().getTransactions()[0].getStatus());
+    Assert.assertEquals("Transaction merchant ID is not as expected", "ScanGoSuper", transactionsResponse.getData().getTransactions()[0].getMerchantId());
+    Assert.assertEquals("Transaction amount is not as expected", Integer.parseInt(TestProperties.get("DIGIPAY_TRANSACTION_AMOUNT")), transactionsResponse.getData().getTransactions()[0].getGrossAmount());
 
-    Assert.assertEquals("Rewards ID number is not as expected", Integer.parseInt(TestProperties.get("DIGIPAY_TRANSACTION_AMOUNT")), transactionsResponse.getData().getTransactions()[0].getInstruments()[0].getAmount());
-    Assert.assertEquals("Rewards ID number is not as expected", instrument, transactionsResponse.getData().getTransactions()[0].getInstruments()[0].getPaymentInstrumentId());
-    Assert.assertEquals("Rewards ID number is not as expected", 16, transactionsResponse.getData().getTransactions()[0].getInstruments()[0].getPaymentTransactionRef().length());
+    Assert.assertEquals("Transaction instrument amount is not as expected", Integer.parseInt(TestProperties.get("DIGIPAY_TRANSACTION_AMOUNT")), transactionsResponse.getData().getTransactions()[0].getInstruments()[0].getAmount());
+    Assert.assertEquals("Transaction instrument ID is not as expected", instrument, transactionsResponse.getData().getTransactions()[0].getInstruments()[0].getPaymentInstrumentId());
+    Assert.assertEquals("Transaction reference is not as expected", 16, transactionsResponse.getData().getTransactions()[0].getInstruments()[0].getPaymentTransactionRef().length());
 
-    Assert.assertEquals("Rewards ID number is not as expected", "00", transactionsResponse.getData().getTransactions()[0].getStatusDetail().getCreditCards()[0].getExternalServiceCode());
-    Assert.assertEquals("Rewards ID number is not as expected", instrument, transactionsResponse.getData().getTransactions()[0].getStatusDetail().getCreditCards()[0].getPaymentInstrumentId());
-    Assert.assertEquals("Rewards ID number is not as expected", "APPROVED", transactionsResponse.getData().getTransactions()[0].getStatusDetail().getCreditCards()[0].getExternalServiceMessage());
-    Assert.assertNull("Rewards ID number is not as expected", transactionsResponse.getData().getTransactions()[0].getStatusDetail().getFraudResponse().getReasonCode());
+    Assert.assertEquals("Transaction status detail external service code is not as expected", "00", transactionsResponse.getData().getTransactions()[0].getStatusDetail().getCreditCards()[0].getExternalServiceCode());
+    Assert.assertEquals("Transaction status detail payment instrument ID is not as expected", instrument, transactionsResponse.getData().getTransactions()[0].getStatusDetail().getCreditCards()[0].getPaymentInstrumentId());
+    Assert.assertEquals("Transaction status detail external service message is not as expected", "APPROVED", transactionsResponse.getData().getTransactions()[0].getStatusDetail().getCreditCards()[0].getExternalServiceMessage());
+    Assert.assertNull("Transaction status detail fraud reason code is null", transactionsResponse.getData().getTransactions()[0].getStatusDetail().getFraudResponse().getReasonCode());
 
-    Assert.assertNotNull("Rewards ID number is not as expected", transactionsResponse.getData().getTransactions()[0].getExecutionTime());
-    Assert.assertNotNull("Rewards ID number is not as expected", transactionsResponse.getData().getTransactions()[0].getTransactionId());
-    Assert.assertNotNull("Rewards ID number is not as expected", transactionsResponse.getData().getTransactions()[0].getClientReference());
-    Assert.assertNotNull("Rewards ID number is not as expected", transactionsResponse.getData().getTransactions()[0].getPaymentRequestId());
-    Assert.assertNotNull("Rewards ID number is not as expected", transactionsResponse.getData().getTransactions()[0].getMerchantReferenceId());
+    Assert.assertNotNull("Transaction execution time is null", transactionsResponse.getData().getTransactions()[0].getExecutionTime());
+    Assert.assertNotNull("Transaction ID is null", transactionsResponse.getData().getTransactions()[0].getTransactionId());
+    Assert.assertNotNull("Transaction client reference is null", transactionsResponse.getData().getTransactions()[0].getClientReference());
+    Assert.assertNotNull("Transaction payment request ID is null", transactionsResponse.getData().getTransactions()[0].getPaymentRequestId());
+    Assert.assertNotNull("Transaction merchant reference ID is null", transactionsResponse.getData().getTransactions()[0].getMerchantReferenceId());
 
-    Assert.assertEquals("Rewards ID number is not as expected", 1, transactionsResponse.getMeta().getRecordCount());
-    Assert.assertEquals("Rewards ID number is not as expected", 1, transactionsResponse.getMeta().getTotalRecords());
+    Assert.assertEquals("Transaction record count value is not as expected", 1, transactionsResponse.getMeta().getRecordCount());
+    Assert.assertEquals("Transaction total records value is not as expected", 1, transactionsResponse.getMeta().getTotalRecords());
   }
 }
