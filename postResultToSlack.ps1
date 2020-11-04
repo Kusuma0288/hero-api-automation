b29119b6d7f99ee1f6a6e449096b220f59b2f456
Jas Bamrah <jbamrah@woolworths.com.au>
2020-11-04 23:14:15 +0000

    Merged PR 25468: Pipeline rule to check failure count
    This PR includes:
    1. Variable group added to apigee and trader pipelines
    2. Check to compare the failed count with previous failed count using variable values
    
    For storing the variable values, we are not using static variable. This is a dynamic variable for which value will be updated for every run. Variables are maintained here.
    https://wowonline.visualstudio.com/Mobile/_apps/hub/ms.vss-distributed-task.hub-library?itemType=VariableGroups&view=VariableGroupView&variableGroupId=717&path=MobileAuto-UAT
    
    Apigee_UAT_LastFailCount: variable will be updated if the run is for apigee in UAT env
    Trader_UAT_LastFailCount: variable will be updated if the run is for trader in UAT env

---
+++ postResultToSlack.ps1
@@ -0,0 +1,92 @@
+Param([string]$ClusterUrl, [string]$Service, [string]$RunEnv, [string]$BuildNumber, [string] $TestRunTitle, [string] $SlackWebhookUrl, [string] $SourceBranchName)
+
+$baseUrl = $ClusterUrl + "/" + $Service + "_" + $RunEnv + "_" + $BuildNumber
+echo 'baseURL:'$baseUrl
+
+$testResultDetailsUrl = $baseUrl + "/index.html"
+$imageURL = $baseUrl + "/advanced-reports/test.png"
+$wolvesURL = $baseUrl + "/pages/tag-scenarios/tag_Wolves.html"
+$lionURL = $baseUrl + "/pages/tag-scenarios/tag_LION.html"
+$lobsterURL = $baseUrl + "/pages/tag-scenarios/tag_Lobsters.html"
+$vegemiteURL = $baseUrl + "/pages/tag-scenarios/tag_VEGEMITE.html"
+$nutellaURL = $baseUrl + "/pages/tag-scenarios/tag_NUTELLA.html"
+$cloverURL = $baseUrl + "/pages/tag-scenarios/tag_CLOVER.html"
+$totalURL = $baseUrl + "/pages/tag-scenarios/tag_REGRESSION_APIGEE.html"
+
+##functions
+function calculation {
+ param ([string]$value)
+
+ $pattern='<li .*class=\s*"list-group-item"[^>]*>(?:.|\n)+?</li>'
+ $pattern11='<i[^>]*>(?:.|\n)+?</i>'
+ try
+ {
+   $webclient = New-Object System.Net.WebClient
+   $html = $webclient.DownloadString($value)
+   $Regex = [Regex]::Matches($html, $pattern)
+   $newVal = $Regex.value
+   $pattern11='<i[^>]*>(?:.|\n)+?</i>'
+   $Regex1 = [Regex]::Matches($newVal, $pattern11)
+   $count = $Regex1.value.split('title="passed"></i>')[1].Trim().Substring(0,2)
+ }
+ catch{
+   #"No feature/scenario exists for the tag provided"
+   $count = 0
+ }
+   return $count
+}
+
+$wolvesCount = calculation $wolvesURL
+$lionCount = calculation $lionURL
+$lobsterCount = calculation $lobsterURL
+$vegemiteCount = calculation $vegemiteURL
+$cloverCount = calculation $cloverURL
+$nutellaCount = calculation $nutellaURL
+$total_failures = calculation $totalURL
+
+if($SourceBranchName -ne 'merge')
+{
+    $color = 'good';
+    $slackMessage = @{
+    attachments = @(
+    @{
+       title = $TestRunTitle
+       color = $color
+       title_link = $testResultDetailsUrl
+       fields = @(
+          @{title = 'Source Branch'; value = $SourceBranchName ; short = 'true'},
+          @{title = 'Run Env'; value = $RunEnv; short = 'true'}
+          @{title = 'Failures:'; short = 'false'}
+          @{title = ' '; short = 'false'}
+          if($wolvesCount -ne 0){
+            @{title = 'Wolves'; value = $wolvesCount; short = 'true'}
+          }
+          if($lionCount -ne 0){
+            @{title = 'Lion'; value = $lionCount; short = 'true'}
+          }
+          if($lobsterCount -ne 0){
+            @{title = 'Lobsters'; value = $lobsterCount; short = 'true'}
+          }
+          if($vegemiteCount -ne 0){
+                @{title = 'Vegemite'; value = $vegemiteCount; short = 'true'}
+          }
+          if($nutellaCount -ne 0){
+                @{title = 'Nutella'; value = $nutellaCount; short = 'true'}
+          }
+          if($cloverCount -ne 0){
+                @{title = 'Clover'; value = $cloverCount; short = 'true'}
+          }
+        )
+     image_url = $imageURL
+    }
+    )
+    } | ConvertTo-Json -Depth 5
+
+    Invoke-RestMethod -Uri $SlackWebhookUrl `
+        -Method Post `
+        -Body $slackMessage `
+        -UseBasicParsing `
+        -ContentType 'application/json'
+}
+$result = @{ currentFailures = $total_failures; reportURL = $testResultDetailsUrl; }
+return $result
