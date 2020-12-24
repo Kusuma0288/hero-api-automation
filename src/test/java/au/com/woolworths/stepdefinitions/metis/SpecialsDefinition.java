8a4ddacca4d96bf698afe6dcf9d6b9f3c90e74fd
Pramod Tewarie <ptewarie@woolworths.com.au>
2020-12-24 01:06:45 +0000

    Merged PR 28320: CLO-2238 specials sort
    ### Description of what this PR contains or how it fixes the issue:
    tests for calling specials with sorts. Currently, tested this with production data. In the future we will need to update the card number, once we have a user with favourites in the test environment. For now, this test is using the default card.
    
    ### Link to JIRA ticket with brief summary:
    https://woolworthsdigital.atlassian.net/browse/CLO-2238
    
    ### Screenshots:
    ![image.png](https://wowonline.visualstudio.com/2c47de12-5d5b-435d-8005-74afeb496636/_apis/git/repositories/7cb3b87c-785a-4d60-b22a-bd54c1480cf6/pullRequests/28320/attachments/image.png)
    
    Regression Trader:
    ![image (3).png](https://wowonline.visualstudio.com/2c47de12-5d5b-435d-8005-74afeb496636/_apis/git/repositories/7cb3b87c-785a-4d60-b22a-bd54c1480cf6/pullRequests/28320/attachments/image%20%283%29.png)
    
    ### Known issues with pull request:
    
    ### Fix Version:
    1.17
    ### Impacted areas of application:
    Favourites
    
    ### Does this PR contain a reusable code (or potentially can be moved to a standalone function)?

--- src/test/java/au/com/woolworths/stepdefinitions/metis/SpecialsDefinition.java
+++ src/test/java/au/com/woolworths/stepdefinitions/metis/SpecialsDefinition.java
@@ -9,15 +9,15 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import junit.framework.Assert;
 
-
 import java.io.IOException;
 import java.util.List;
 import java.util.logging.Logger;
 
 public class SpecialsDefinition extends SpecialsHelper {
 
-  private SpecialsResponse response;
   private final static Logger logger = Logger.getLogger("SpecialsDefinition.class");
+  private SpecialsResponse response;
+  private String sortOptionWithUnderscore;
 
   @When("^the user selects his favourites products$")
   public void theUserSelectsHisFavouritesProducts() throws IOException {
@@ -25,8 +25,16 @@ public class SpecialsDefinition extends SpecialsHelper {
     logger.info("Showing favourite products");
   }
 
+  @When("^the user sorts his favourites products by (.*)$")
+  public void theUserSortsHisFavouritesProducts(String sortOption) throws IOException {
+    sortOptionWithUnderscore = sortOption.toUpperCase().replace(" ", "_");
+    response = getSpecialsSortedResponse(sortOptionWithUnderscore);
+    logger.info("Showing favourite products sorted by " + sortOption);
+  }
+
   @Then("^the user should see all his favourites product$")
   public void theUserShouldSeeAllHisFavouritesProduct() {
+    Assert.assertEquals("status code", "200", response.getStatusCode());
 
     for (int j = 0; j < response.getData().getPartners().size(); j++) {
       List<Product> products = response.getData().getPartners().get(j).getData().getProducts();
@@ -40,7 +48,7 @@ public class SpecialsDefinition extends SpecialsHelper {
   }
 
   @And("^if no products are present the user should see a message$")
-  public void ifNoProductsArePresentTheUserShouldSeeAMessage() {
+  public boolean areProductsPresent() {
 
     for (int i = 0; i < response.getData().getPartners().size(); i++) {
       Partner partner = response.getData().getPartners().get(i);
@@ -49,6 +57,7 @@ public class SpecialsDefinition extends SpecialsHelper {
         Assert.assertNotNull("Partner title is not shown ", partner.getData().getMessage().getData().getTitle());
       }
     }
+    return true;
   }
 
   @And("^if the partner is Woolworths it should display the action button$")
@@ -59,4 +68,55 @@ public class SpecialsDefinition extends SpecialsHelper {
       Assert.assertNotNull("Woolworths title is not shown", woolworthsPartner.getData().getMessage().getData().getTitle());
     }
   }
+
+  @Then("^the product should be sorted by Sort Option$")
+  public void theProductShouldBeSortedBySortOption() {
+    if (areProductsPresent()) {
+      return;
+    }
+    Assert.assertEquals("status code", "200", response.getStatusCode());
+
+    List<Product> products = response.getData().getPartners().get(0).getData().getProducts();
+    switch (sortOptionWithUnderscore) {
+      case "PRICE_LOW_TO_HIGH":
+        isSortedByPrice(products, true);
+        break;
+      case "PRICE_HIGH_TO_LOW":
+        isSortedByPrice(products, false);
+        break;
+      case "NAME_A_TO_Z":
+        isSortedByName(products, false);
+        break;
+      case "NAME_Z_TO_A":
+        isSortedByName(products, true);
+        break;
+    }
+
+
+  }
+
+  public void isSortedByPrice(List<Product> products, boolean ascending) {
+    for (int i = 1; i < products.size(); i++) {
+      double price = products.get(i - 1).getData().getPrice();
+      double nextPrice = products.get(i).getData().getPrice();
+      if (ascending) {
+        Assert.assertTrue("Favourites is not sorted 0-9", price <= nextPrice);
+      } else {
+        Assert.assertTrue("Favourites is not sorted 9-0", price >= nextPrice);
+      }
+    }
+  }
+
+  public void isSortedByName(List<Product> products, boolean ascending) {
+    for (int i = 1; i < products.size(); i++) {
+      String name = products.get(i - 1).getData().getName();
+      String nextName = products.get(i).getData().getName();
+      if (ascending) {
+        Assert.assertTrue("Favourites is not sorted A-Z", name.compareToIgnoreCase(nextName) >= 0);
+      } else {
+        Assert.assertTrue("Favourites is not sorted Z-A", name.compareToIgnoreCase(nextName) <= 0);
+      }
+    }
+  }
+
 }
