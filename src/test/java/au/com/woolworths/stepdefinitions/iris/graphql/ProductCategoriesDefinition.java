c16ff8428b921ec30273ae01d5e8ecd67ef8d2b7
Naeem Raza <nraza@woolworths.com.au>
2021-03-18 13:25:19 +1100

    Some more Lint issues

--- src/test/java/au/com/woolworths/stepdefinitions/iris/graphql/ProductCategoriesDefinition.java
+++ src/test/java/au/com/woolworths/stepdefinitions/iris/graphql/ProductCategoriesDefinition.java
@@ -15,31 +15,31 @@ public class ProductCategoriesDefinition extends BaseHelper {
   ObjectMapper mapper = new ObjectMapper();
   public GraphqlHelper graphqlHelper = new GraphqlHelper();
 
-    @When("^user requests for \"([^\"]*)\" sub-categories for store: \"([^\"]*)\"$")
+  @When("^user requests for \"([^\"]*)\" sub-categories for store: \"([^\"]*)\"$")
     public void getAvailableProductCategories(String categoryType, String storeId) throws Throwable {
-        if (sharedData.productCategories == null) {
+    if (sharedData.productCategories == null) {
             // First call
-            sharedData.productCategories = getAvailableProductCategoriesNextLevel(categoryType, "", storeId);
-        } else {
-            sharedData.productCategories = getAvailableProductCategoriesNextLevel(categoryType, sharedData.productCategories, storeId);
-        }
+      sharedData.productCategories = getAvailableProductCategoriesNextLevel(categoryType, "", storeId);
+    } else {
+      sharedData.productCategories = getAvailableProductCategoriesNextLevel(categoryType, sharedData.productCategories, storeId);
     }
+  }
 
-    private String getAvailableProductCategoriesNextLevel(String categoryType, String SubCategory, String StoreID) throws Throwable {
-        InputStream iStream = ProductCategoriesDefinition.class.getResourceAsStream("/gqlQueries/iris/productCategories.graphql");
-        variables.put(ProductCategoriesResponseHelper.ProductsByCategoriesArgs.CATEGORY_ID.get(), SubCategory);
-        variables.put(ProductCategoriesResponseHelper.ProductsByCategoriesArgs.CATEGORIES_TYPE.get(), categoryType);
-        variables.put(ProductCategoriesResponseHelper.ProductsByCategoriesArgs.STORE_ID.get(), StoreID);
-        String productsByCategoriesQuery = GraphqlParser.parseGraphql(iStream, variables);
-        String productsByCategoriesResponseString = graphqlHelper.postGraphqlQuery(productsByCategoriesQuery);
-        ProductCategoriesResponse productCategoriesResponse = mapper.readValue(productsByCategoriesResponseString, ProductCategoriesResponse.class);
+  private String getAvailableProductCategoriesNextLevel(String categoryType, String SubCategory, String StoreID) throws Throwable {
+    InputStream iStream = ProductCategoriesDefinition.class.getResourceAsStream("/gqlQueries/iris/productCategories.graphql");
+    variables.put(ProductCategoriesResponseHelper.ProductsByCategoriesArgs.CATEGORY_ID.get(), SubCategory);
+    variables.put(ProductCategoriesResponseHelper.ProductsByCategoriesArgs.CATEGORIES_TYPE.get(), categoryType);
+    variables.put(ProductCategoriesResponseHelper.ProductsByCategoriesArgs.STORE_ID.get(), StoreID);
+    String productsByCategoriesQuery = GraphqlParser.parseGraphql(iStream, variables);
+    String productsByCategoriesResponseString = graphqlHelper.postGraphqlQuery(productsByCategoriesQuery);
+    ProductCategoriesResponse productCategoriesResponse = mapper.readValue(productsByCategoriesResponseString, ProductCategoriesResponse.class);
 
-        Assert.assertNotNull(productCategoriesResponse.getData().getProductCategories().getCategories().get(0).getTitle());
-        Assert.assertNotNull(productCategoriesResponse.getData().getProductCategories().getCategories().get(0).getIsFinalLevel());
-        Assert.assertNotNull(productCategoriesResponse.getData().getProductCategories().getCategories().get(0).getCategoryId());
+    Assert.assertNotNull(productCategoriesResponse.getData().getProductCategories().getCategories().get(0).getTitle());
+    Assert.assertNotNull(productCategoriesResponse.getData().getProductCategories().getCategories().get(0).getIsFinalLevel());
+    Assert.assertNotNull(productCategoriesResponse.getData().getProductCategories().getCategories().get(0).getCategoryId());
 
-        if (productCategoriesResponse.getData().getProductCategories().getCategories().get(0).getIsFinalLevel()) {
-            return null;
-        } else return productCategoriesResponse.getData().getProductCategories().getCategories().get(0).getCategoryId();
-    }
+    if (productCategoriesResponse.getData().getProductCategories().getCategories().get(0).getIsFinalLevel()) {
+      return null;
+    } else return productCategoriesResponse.getData().getProductCategories().getCategories().get(0).getCategoryId();
+  }
 }
