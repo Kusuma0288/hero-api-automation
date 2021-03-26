ff72c7c23552f42b2df032958f60e63fb0c17d12
Lewis O'Brien <lobrien1@woolworths.com.au>
2021-03-26 15:59:52 +1100

    Use pretty format

--- src/test/java/au/com/woolworths/RunCucumberTest.java
+++ src/test/java/au/com/woolworths/RunCucumberTest.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/features/"}, glue = "au.com.woolworths.stepdefinitions", plugin = {"json:target/cucumber-reports/cucumber.json"})
+@CucumberOptions(features = {"src/test/resources/features/"}, glue = "au.com.woolworths.stepdefinitions", plugin = {"json:target/cucumber-reports/cucumber.json", "pretty"})
 public class RunCucumberTest {
 }
