0aa8f4327697255da4113df44159665dc70d4eca
shobhavelu <svajravelu@woolworths.com.au>
2021-03-30 12:39:29 +1100

    added base64 method

--- src/test/java/au/com/woolworths/helpers/metis/ApplePayHelper.java
+++ src/test/java/au/com/woolworths/helpers/metis/ApplePayHelper.java
@@ -8,24 +8,30 @@ import au.com.woolworths.utils.URLResources;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.util.Base64;
 import java.util.Map;
 
 public class ApplePayHelper extends BaseHelper {
-    RestInvocationUtil invocationUtil;
+  RestInvocationUtil invocationUtil;
 
-    public ApplePayHelper() {
-        this.invocationUtil = ServiceHooks.restInvocationUtil;
-    }
+  public ApplePayHelper() {
+    this.invocationUtil = ServiceHooks.restInvocationUtil;
+  }
 
-    public AppleWalletResponse iAddToAppleWallet(String query) throws IOException {
+  public AppleWalletResponse iAddToAppleWallet(String query) throws IOException {
 
-        ObjectMapper mapper = new ObjectMapper();
-        Map <String, String> mapWebserviceResponse;
+    ObjectMapper mapper = new ObjectMapper();
+    Map<String, String> mapWebserviceResponse;
 
-        String endPoint = URLResources.METIS_REWARDS_GRAPHQL;
+    String endPoint = URLResources.METIS_REWARDS_GRAPHQL;
 
-        mapWebserviceResponse = invocationUtil.invokePostWithHeaders(endPoint, query, headerListRewards);
-        String responseStr = mapWebserviceResponse.get("response");
-        return mapper.readValue(responseStr, AppleWalletResponse.class);
-    }
+    mapWebserviceResponse = invocationUtil.invokePostWithHeaders(endPoint, query, headerListRewards);
+    String responseStr = mapWebserviceResponse.get("response");
+    return mapper.readValue(responseStr, AppleWalletResponse.class);
+  }
+
+  public String base64decode(String file) {
+    byte[] decoder = Base64.getDecoder().decode(file);
+    return new String(decoder);
+  }
 }
--- src/test/java/au/com/woolworths/stepdefinitions/metis/ApplePayDefinition.java
+++ src/test/java/au/com/woolworths/stepdefinitions/metis/ApplePayDefinition.java
@@ -11,22 +11,29 @@ import java.io.InputStream;
 import java.util.logging.Logger;
 
 public class ApplePayDefinition extends ApplePayHelper {
-    private final static Logger logger = Logger.getLogger("ApplePayDefinition.class");
-    private AppleWalletResponse AppleWalletResponse = new AppleWalletResponse();
+  private final static Logger logger = Logger.getLogger("ApplePayDefinition.class");
+  private AppleWalletResponse AppleWalletResponse = new AppleWalletResponse();
 
-    @When("^the user adds the rewards card to Apple Pay wallet")
-    public void theUserSelectsApplePay() throws Throwable {
-        InputStream iStream = ApplePayHelper.class.getResourceAsStream("/gqlQueries/metis/queries/rewards/appleWallet.graphql");
+  @When("^the user adds the rewards card to Apple pay wallet$")
+  public void theUserAddsTheRewardsCardToApplePayWallet() throws Throwable {
+    InputStream iStream = ApplePayHelper.class.getResourceAsStream("/gqlQueries/metis/queries/rewards/appleWallet.graphql");
+    String graphqlQuery = GraphqlParser.parseGraphql(iStream, null);
+    AppleWalletResponse = iAddToAppleWallet(graphqlQuery);
+  }
 
-        String graphqlQuery = GraphqlParser.parseGraphql(iStream, null);
-        AppleWalletResponse = iAddToAppleWallet(graphqlQuery);
-    }
+  @Then("^the rewards card is saved to Apple Pay")
+  public void activityDetailsAreDisplayed() {
+    String file = AppleWalletResponse.getData().getWalletPkPass().getFile();
+    String message = base64decode(file);
+    Assert.assertNotNull("the file token should not be empty", file);
+    Assert.assertTrue("the icon should be present", message.contains("icon.png"));
+    Assert.assertTrue("the logo should be present", message.contains("logo.png"));
+    Assert.assertTrue("the 2X-logo should be present", message.contains("logo@2x.png"));
+    Assert.assertTrue("the manifest json should be present", message.contains("manifest.json"));
+    Assert.assertTrue("the pass json should be present", message.contains("pass.json"));
+    Assert.assertTrue("the strip should be present", message.contains("strip.png"));
+    Assert.assertTrue("the 2X-strip should be present", message.contains("strip@2x.png"));
+  }
 
 
-    @Then("^the rewards card is saved to Apple Pay")
-    public void activityDetailsAreDisplayed() {
-        String file = AppleWalletResponse.getData().getWalletPkPass().getFile();
-        logger.info("Showing the connect Token: " + file);
-        Assert.assertNotNull("the connect token should not be empty", file);
-    }
 }
