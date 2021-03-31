c49076b6335a76f010f86eea79b11115b5a2b588
Lewis O'Brien <lobrien1@woolworths.com.au>
2021-03-31 16:26:13 +1100

    Fix

--- postResultToSlack.ps1
+++ postResultToSlack.ps1
@@ -28,7 +28,7 @@ function calculation {
    $newVal = $Regex.value
    $pattern11='<i[^>]*>(?:.|\n)+?</i>'
    $Regex1 = [Regex]::Matches($newVal, $pattern11)
-   $count = $Regex1.value.split("='anchor-failed';")[1].Trim()
+   $count = $Regex1.value.split("='anchor-failed';")[0].Trim()
  }
  catch{
    #"No feature/scenario exists for the tag provided"
