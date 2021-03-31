1f027a72bcf27138e922b938a9ec2766357d0386
Lewis O'Brien <lobrien1@woolworths.com.au>
2021-03-31 22:13:11 +1100

    Remove trim

--- postResultToSlack.ps1
+++ postResultToSlack.ps1
@@ -28,7 +28,7 @@ function calculation {
    $newVal = $Regex.value
    $pattern11='<i[^>]*>(?:.|\n)+?</i>'
    $Regex1 = [Regex]::Matches($newVal, $pattern11)
-   $count = $Regex1.value.split("='anchor-failed';`">")[1].Trim()
+   $count = $Regex1.value.split("='anchor-failed';`">")[1]
  }
  catch{
    #"No feature/scenario exists for the tag provided"
