4a0d4c6d7b23e23377ceda23b30ac21b90bb56c5
JasWoolieX <jbamrah@woolworths.com.au>
2021-04-13 09:36:40 +1000

    Update postResultToSlack.ps1

--- postResultToSlack.ps1
+++ postResultToSlack.ps1
@@ -15,7 +15,6 @@ $cloverURL = $baseUrl + "/pages/tag-scenarios/tag_CLOVER.html"
 $falconURL = $baseUrl + "/pages/tag-scenarios/tag_Falcon.html"
 $manukaURL = $baseUrl + "/pages/tag-scenarios/tag_Manuka.html"
 $totalURL = $baseUrl + "/pages/tag-scenarios/tag_" + $totalTagUrl + ".html"
-##$totalURL = $baseUrl + "/pages/tag-scenarios/tag_REGRESSION_TRADER.html"
 
 ##functions
 function calculation {
