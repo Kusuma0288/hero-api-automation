153eb24f5143f6f78752c0e08760c87cbc27c307
Veerasurya <vramasamy2@woolworths.com.au>
2021-06-18 15:04:36 +1000

    add intervention scenarios

--- src/test/java/au/com/woolworths/stepdefinitions/scango/KioskCheckoutDefinition.java
+++ src/test/java/au/com/woolworths/stepdefinitions/scango/KioskCheckoutDefinition.java
@@ -1,9 +1,13 @@
 package au.com.woolworths.stepdefinitions.scango;
 
 import au.com.woolworths.helpers.scango.CheckoutHelper;
+import au.com.woolworths.model.scango.errorresponse.KioskErrorResponse;
 import au.com.woolworths.model.scango.kiosk.KioskCheckoutResponse;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.testng.Assert;
+
+import java.io.IOException;
 
 public class KioskCheckoutDefinition extends CheckoutHelper {
 
@@ -14,5 +18,13 @@ public class KioskCheckoutDefinition extends CheckoutHelper {
     sharedData.responseStatusCode = kioskCheckoutResponse.getStatusCode();
 
     Assert.assertTrue(sharedData.responseStatusCode.contains("200"), "Connection not successful::" + sharedData.responseStatusCode);
+  }
+
+  @Then("I verify Kiosk Checkout API throws valid error response")
+  public void iVerifyKioskCheckoutAPINoValidItemsErrorResponse() throws IOException {
+    KioskErrorResponse kioskErrorResponse = iClickOnKioskCheckoutNoValidErrorResponse();
+    Assert.assertTrue(kioskErrorResponse.getMessage().equalsIgnoreCase("NoValidItemsInCart"));
+    Assert.assertTrue(kioskErrorResponse.getDescription().equalsIgnoreCase("There are no other items to purchase in this transaction. No payment at Kiosk required."));
+
   }
 }
